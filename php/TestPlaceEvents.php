680f95cddd650220d8795d9cb0be15e896efd405
Sakhile <22sakhile@gmail.com>
2020-06-13 17:07:26 +0200

    configuring tests

---
+++ php/TestPlaceEvents.php
@@ -0,0 +1,19 @@
+<?php
+
+// require 'place_events.php';
+// echo $company_name;
+class TestPlaceEvents extends PHPUnit\Framework\TestCase {
+    
+    public function testInputFields() {
+
+        include_once 'place_events.php';
+
+        $this->assertEquals($company_name, 'Coronation Fund Managers');
+        $this->assertEquals($name_contact_person, 'Sakhile Mabunda');
+        $this->assertEquals($email_address, 'selebrate22@gmail.com');
+        $this->assertEquals($phone_number, '0715398516');
+    }
+
+}
+
+?>
